b7f5886376369cb4aff2e06a9ce70512480acf54
digit4 <dhaval.pantojee@gmail.com>
2025-04-23 12:56:12 +0530

    added some mappings
    - snipe mappings
    - harpoon mappings
    - some basic ones for easier typing

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,53 @@
 require "nvchad.mappings"
+local snipe = require "snipe"
 
 -- add yours here
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "<C-CR>", "<End><CR>", { desc = "C-CR moves the cursor to new line"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local harpoon = require "harpoon"
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+-- vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+harpoon:extend({
+  UI_CREATE = function(cx)
+    vim.keymap.set("n", "<C-v>", function()
+      harpoon.ui:select_menu_item({ vsplit = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-x>", function()
+      harpoon.ui:select_menu_item({ split = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon.ui:select_menu_item({ tabedit = true })
+    end, { buffer = cx.bufnr })
+  end,
+})
+
+local harpoon_extensions = require("harpoon.extensions")
+harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+
+-- Snipe mappings
+
+    map( "n","<leader><leader>", function () snipe.open_buffer_menu() end, { desc = "Open Snipe buffer menu" } )
+
