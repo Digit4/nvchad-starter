3eb94e89b5d40dbfa0f83aa75d1f9d1562fd0c69
Dhaval Pantojee <dhaval.atul.pantojee@accenture.com>
2025-05-01 00:25:07 +0530

    blink changes + formatting

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,18 +1,25 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  "saghen/blink.cmp",
+  event = "VimEnter",
+  -- dependencies = {  'L3MON4D3/LuaSnip',version = '2.*',build =(function()
+  --   if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+  --           return
+  --         end
+  --         return 'make install_jsregexp'
+  -- ) ,
+  dependencies = { "rafamadriz/friendly-snippets" },
 
-  version = '1.*',
+  version = "1.*",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { "lsp", "path", "snippets", "buffer" },
     },
 
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+    fuzzy = { implementation = "prefer_rust" },
 
-    completion = { menu = { auto_show = true },  documentation = { auto_show = false } },
+    completion = { menu = { auto_show = true }, documentation = { auto_show = false } },
 
     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
     -- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -26,13 +33,13 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'super-tab' },
+    keymap = { preset = "super-tab" },
 
     appearance = {
-      nerd_font_variant = 'mono'
+      nerd_font_variant = "mono",
     },
 
     -- (Default) Only show the documentation popup when manually triggered
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
