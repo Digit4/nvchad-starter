178980c019854687860b6ccef931b7271339f6c6
Dhaval Pantojee <dhaval.atul.pantojee@accenture.com>
2025-04-30 23:53:31 +0530

    Changed conform
    - removed async from format onasave as it's invalid.
    - increased timeout to 10s.
    - changed prettie

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,4 +1,5 @@
 local options = {
+  lsp_fallback = true,
   formatters_by_ft = {
     lua = { "stylua" },
     css = { "prettierd" },
@@ -11,7 +12,8 @@ local options = {
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 10000,
+    -- async = true,
     lsp_fallback = true,
   },
 }
