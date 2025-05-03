30cd84f8f9da7752e8cc35ed1ff009ec484e93f9
Dhaval Pantojee <dhaval.atul.pantojee@accenture.com>
2025-05-03 17:08:45 +0530

    added treesitter textobjects

---
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  lazy = true,
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      textobjects = {
+        enable = true,
+
+        lookahead = true,
+
+        keymaps = {
+          ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment." },
+          ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment." },
+          ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment." },
+          ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment." },
+        },
+      },
+    }
+  end,
+}
